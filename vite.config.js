9d4aac6301bc57b73b33d1e9162ca07f0e3e9075
Albinoni <albinoni1423@gmail.com>
2024-10-24 03:24:04 -0300

    added PDFViewer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vite';
+import wasm from 'vite-plugin-wasm';
 import react from '@vitejs/plugin-react'
+import topLevelAwait from 'vite-plugin-wasm';
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    wasm(),
+    topLevelAwait(),
+  ],
+});
